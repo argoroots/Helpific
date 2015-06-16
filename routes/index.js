7b30180ea7f60fbe86758fad7e53bb6d30c5eba1
Argo Roots <argo@roots.ee>
2015-06-16 18:48:20 +0300

    Set page title

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ var debug   = require('debug')('app:' + path.basename(__filename).replace('.js',
 
 // GET home page
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Helpific'
-    })
+    res.render('index')
 })
 
 
