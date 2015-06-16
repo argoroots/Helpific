c8a2289c85e7869511b6c3c9b756c57132b35999
Argo Roots <argo@roots.ee>
2015-06-17 02:51:44 +0300

    Profile image and text

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,8 +1,9 @@
-var express     = require('express')
-var router      = express.Router()
-var path        = require('path')
-var debug       = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
-var request     = require('request')
+var express = require('express')
+var router  = express.Router()
+var path    = require('path')
+var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
+var request = require('request')
+var md      = require('marked')
 
 
 
@@ -29,9 +30,49 @@ router.get('/:id', function(req, res, next) {
         if(error) throw error
         if(response.statusCode !== 200 || !body.result) throw new Error(body)
 
-        var profile = body.result
+        var properties = body.result.properties
+        var profile = {
+            forename: '',
+            surname: '',
+            about: {
+                text: '',
+                photo: '',
+                video: ''
+            },
+            i_help: {
+                text: '',
+                photo: '',
+                video: ''
+            },
+            you_help: {
+                text: '',
+                photo: '',
+                video: ''
+            },
+
+        }
+
+        if(properties['forename'].values) profile.forename = properties['forename'].values[0].db_value
+        if(properties['surname'].values) profile.surname = properties['surname'].values[0].db_value
+        if(properties['slogan'].values) profile.slogan = properties['slogan'].values[0].db_value
+        if(properties['photo'].values) profile.photo = APP_ENTU_URL + '/file-' + properties['photo'].values[0].db_value
+
+        if(properties['about-me-text'].values) profile.about.text = properties['about-me-text'].values[0].db_value
+        if(properties['about-me-photo'].values) profile.about.photo = properties['about-me-photo'].values[0].db_value
+        if(properties['about-me-video'].values) profile.about.video = properties['about-me-video'].values[0].db_value
+
+        if(properties['me-help-you-text'].values) profile.i_help.text = properties['me-help-you-text'].values[0].db_value
+        if(properties['me-help-you-photo'].values) profile.i_help.photo = properties['me-help-you-photo'].values[0].db_value
+        if(properties['me-help-you-video'].values) profile.i_help.video = properties['me-help-you-video'].values[0].db_value
+
+        if(properties['you-help-me-text'].values) profile.you_help.text = properties['you-help-me-text'].values[0].db_value
+        if(properties['you-help-me-photo'].values) profile.you_help.photo = properties['you-help-me-photo'].values[0].db_value
+        if(properties['you-help-me-video'].values) profile.you_help.video = properties['you-help-me-video'].values[0].db_value
+
         res.render('profile', {
-            title: profile.displayname
+            title: profile.forename + ' ' + profile.surname,
+            profile: profile,
+            md: md
         })
     })
 })
