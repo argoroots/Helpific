9bff563dab1c7642239bdaa14836628d3c23fa3e
Argo Roots <argo@roots.ee>
2015-07-06 14:09:47 +0300

    Get / from Entu

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express')
 var router  = express.Router()
 var path    = require('path')
 var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
+var entu    = require('./entu')
 
 
 
 // GET home page
 router.get('/', function(req, res, next) {
-    res.render('index')
+    entu.get_page(641, function(error, page) {
+        if(error) return next(error)
+        res.render('index', page)
+    })
 })
 
 
