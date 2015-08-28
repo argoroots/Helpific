40e0604266a458786a17c42062ffdadc9a69db60
Argo Roots <argo@roots.ee>
2015-08-28 12:31:01 +0300

    Node cluster

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM node:0.12-slim
 ADD ./ /usr/src/helpific
 RUN cd /usr/src/helpific && npm install
 
-CMD ["node", "/usr/src/helpific/app.js"]
+CMD ["node", "/usr/src/helpific/cluster.js"]
---
+++ cluster.js
@@ -0,0 +1,24 @@
+var cluster = require('cluster')
+var cpuCount = require('os').cpus().length
+
+
+
+cluster.setupMaster({
+    exec: 'app.js',
+})
+
+// Create a worker for each CPU
+for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork()
+}
+
+// Listen for new workers
+cluster.on('online', function(worker) {
+    console.log('Worker ' + worker.id + ' started')
+})
+
+// Listen for dying workers nad replace the dead worker, we're not sentimental
+cluster.on('exit', function(worker) {
+    console.log('Worker ' + worker.id + ' died')
+    cluster.fork()
+})
