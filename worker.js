0a0fd35d03972013f10e1c80cb984e89767e5fd2
Anton.Anikin <anton.anikin@icefire.ee>
2015-11-30 08:26:06 +0200

    ko locale added

--- worker.js
+++ worker.js
@@ -45,7 +45,7 @@ fs.existsSync(APP_CACHE_DIR) || fs.mkdirSync(APP_CACHE_DIR)
 
 // Configure i18n
 i18n.configure({
-    locales: ['en', 'et', 'ru', 'hu', 'de'],
+    locales: ['en', 'et', 'ru', 'hu', 'de', 'ko'],
     defaultLocale: APP_DEFAULT_LOCALE,
     file: path.join(__dirname, 'locales.yaml'),
     updateFile: true
