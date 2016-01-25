fb9893d73ca2e83d69b42fbb960e02f6dbd402fc
Anton.Anikin <anton.anikin@icefire.ee>
2016-01-25 11:33:46 +0200

    french language

--- routes/tr.js
+++ routes/tr.js
@@ -6,7 +6,7 @@ var path    = require('path')
 
 
 
-var newLanguage = ['en', 'et', 'ru', 'hu', 'de', 'ko', 'fi', 'nl', 'cs', 'bg', 'ja', 'zh', 'it']
+var newLanguage = ['en', 'et', 'ru', 'hu', 'de', 'ko', 'fi', 'nl', 'cs', 'bg', 'ja', 'zh', 'it', 'fr']
 
 // Show user own profile
 router.get('/', function(req, res, next) {
