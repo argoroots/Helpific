45cfaf0f33d63fea083cdcd1b05f843c39861d97
Anton.Anikin <anton.anikin@icefire.ee>
2016-02-11 09:36:11 +0200

    cs translation vol4

--- worker.js
+++ worker.js
@@ -52,7 +52,7 @@ fs.existsSync(APP_CACHE_DIR) || fs.mkdirSync(APP_CACHE_DIR)
 
 // Configure i18n
 i18n.configure({
-    locales: ['en', 'et', 'fi', 'fr', 'ru', 'hu', 'de', 'ko', 'zh', 'ja', 'it'],
+    locales: ['en', 'et', 'fi', 'cs', 'fr', 'ru', 'hu', 'de', 'ko', 'zh', 'ja', 'it'],
     defaultLocale: APP_DEFAULT_LOCALE,
     file: path.join(__dirname, 'locales.yaml'),
     updateFile: true,
