a65b7a41d9fe2aafdea8a6e615e530dc19d52c81
Anton.Anikin <anton.anikin@icefire.ee>
2016-04-15 14:47:07 +0300

    greek , lt and lv locales added

--- routes/tr.js
+++ routes/tr.js
@@ -6,7 +6,7 @@ var path    = require('path')
 
 
 
-var newLanguage = ['en', 'et', 'ru', 'hu', 'de', 'ko', 'fi', 'nl', 'cs', 'bg', 'ja', 'zh', 'it', 'fr', 'id']
+var newLanguage = ['en', 'et', 'ru', 'hu', 'de', 'ko', 'fi', 'nl', 'cs', 'bg', 'ja', 'zh', 'it', 'fr', 'id', 'el', 'lt', 'lv']
 
 // Show user own profile
 router.get('/', function(req, res, next) {
