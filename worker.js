6c1983637e969a2a775a96d71da37de03ed1fe82
Anton.Anikin <anton.anikin@icefire.ee>
2016-05-03 15:13:46 +0300

    'el', 'lt', 'lv' added

--- worker.js
+++ worker.js
@@ -60,7 +60,7 @@ require('ssl-root-cas/latest')
 
 // Configure i18n
 i18n.configure({
-    locales: ['en', 'et', 'fi', 'cs', 'fr', 'ru', 'hu', 'de', 'ko', 'zh', 'ja', 'it'],
+    locales: ['en', 'et', 'fi', 'cs', 'fr', 'ru', 'hu', 'de', 'ko', 'zh', 'ja', 'it', 'el', 'lt', 'lv'],
     defaultLocale: APP_DEFAULT_LOCALE,
     file: path.join(__dirname, 'locales.yaml'),
     updateFile: true,
@@ -74,7 +74,10 @@ i18n.configure({
         'cn': 'zh',
         'it': 'it',
         'fi': 'fi',
-        'fr': 'fr'
+        'fr': 'fr',
+        'lv': 'lv',
+        'lt': 'lt',
+        'el': 'el'
     }
 })
 
